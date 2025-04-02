ca9389dc36d5413da1c3915c0128356282f53cc4
Iryna Kosovan <kosovaniryne@gmail.com>
2025-04-03 00:07:44 +0300

    created project

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+$(function() {
+  $(".top__slider").slick({
+    dots: true,
+    arrows: true,
+    prevArrow: '<button class="top__btn top__btn--prev"><img class="top__btn-img" src="images/icons/arrow.svg" alt="Стрілка слайдера наліво"></button>',
+    nextArrow: '<button class="top__btn top__btn--next"><img class="top__btn-img" src="images/icons/arrow.svg" alt="Стрілка слайдера направо"></button>'
+  })
+})
