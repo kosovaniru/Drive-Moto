716a31b10c445f257ac229c50c57b41e3668059d
Iryna Kosovan <kosovaniryne@gmail.com>
2025-04-26 20:28:25 +0300

    created entire index page

--- app/js/main.js
+++ app/js/main.js
@@ -5,20 +5,23 @@ $(function() {
     prevArrow: '<button class="top__btn top__btn--prev"><img class="top__btn-img" src="images/icons/arrow.svg" alt="Стрілка слайдера наліво"></button>',
     nextArrow: '<button class="top__btn top__btn--next"><img class="top__btn-img" src="images/icons/arrow.svg" alt="Стрілка слайдера направо"></button>'
   })
-  $(".tabs__link").on("click", function(e) {
+  $(".tabs__item").on("click", function(e) {
     e.preventDefault();
-    $('.tabs__link').removeClass("tabs__link--active");
-    $('.tabs__content-item').removeClass("tabs__content-item--active");
-    $(this).addClass("tabs__link--active");
-    $($(this).attr('href')).addClass("tabs__content-item--active")
+
+    $($(this).siblings()).removeClass("tabs__item--active");
+    $($(this).parent().siblings().find('.tabs__content-item')).removeClass("tabs__content-item--active");
+    $(this).addClass("tabs__item--active");
+    $($(this).children().attr('href')).addClass("tabs__content-item--active")
   })
 
   $('.offering-card__favorite').on('click', function() {
-    $('.offering-card__favorite').toggleClass('offering-card__favorite--active')
+    $(this).toggleClass('offering-card__favorite--active')
   }),
 
   $(".item-slider").slick({
     arrows: true,
     slidesToShow: 4,
+    prevArrow: '<button class="item-slider__btn item-slider__btn--prev"><img src="images/icons/item-arrow.svg" alt="" class="item-slider__arrow item-slider__arrow--prev"></button>',
+    nextArrow: '<button class="item-slider__btn item-slider__btn--next"><img src="images/icons/item-arrow.svg" alt="" class="item-slider__arrow item-slider__arrow--next"></button>'
   })
 })
